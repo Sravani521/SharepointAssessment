6cecb74b029101b14c3141358be994f4df8351b4
Sravani521 <43287344+Sravani521@users.noreply.github.com>
2018-10-13 17:44:44 +0530

    13oct

--- SharePointAssessment/UploadingFiles/Program.cs
+++ SharePointAssessment/UploadingFiles/Program.cs
@@ -79,37 +79,37 @@ namespace UploadingFiles
                 
                 try
                 {
-                    //specify the file name where its actually exist   
+                   
                     string filepath ="D:/sravani/sharepoint/SharePointAssessment/Book1.xlsx";
 
-                    //open the excel using openxml sdk  
+                   
                     using (SpreadsheetDocument doc = SpreadsheetDocument.Open(filepath, false))
                     {
 
-                        //create the object for workbook part  
+                       
                         WorkbookPart wbPart = doc.WorkbookPart;
 
-                        //statement to get the count of the worksheet  
+                        
                         int worksheetcount = doc.WorkbookPart.Workbook.Sheets.Count();
 
-                        //statement to get the sheet object  
+                     
                         Sheet mysheet = (Sheet)doc.WorkbookPart.Workbook.Sheets.ChildElements.GetItem(0);
 
-                        //statement to get the worksheet object by using the sheet id  
+                    
                         Worksheet Worksheet = ((WorksheetPart)wbPart.GetPartById(mysheet.Id)).Worksheet;
 
-                        //Note: worksheet has 8 children and the first child[1] = sheetviewdimension,....child[4]=sheetdata  
+                       
                         int wkschildno = 4;
 
 
-                        //statement to get the sheetdata which contains the rows and cell in table  
+                      
                         SheetData Rows = (SheetData)Worksheet.ChildElements.GetItem(wkschildno);
 
 
-                        //getting the row as per the specified index of getitem method  
+                        
                         Row currentrow = (Row)Rows.ChildElements.GetItem(0);
 
-                        //getting the cell as per the specified index of getitem method  
+                       
                         Cell currentcell = (Cell)currentrow.ChildElements.GetItem(0);
 
                         string currentcellvalue = string.Empty;
@@ -127,7 +127,7 @@ namespace UploadingFiles
 
                                     if (item.Text != null)
                                     {
-                                        //code to take the string value  
+                                       
                                         currentcellvalue = item.Text.Text;
                                     }
                                     else if (item.InnerText != null)
